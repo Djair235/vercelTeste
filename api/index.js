39c23281016bb55fc40b71eb887ab2265ff7dcb0
Djair235 <djairdecastrofeitosa@gmail.com>
2025-03-01 23:05:14 -0300

    sla

--- api/index.js
+++ api/index.js
@@ -1,6 +1,5 @@
-const path = require("path")
+const path = require("path");
 
 export default function rotaPrincipal(req, res) {
-    // res.status(200).json({message: "ROTA PRINCIPAL FUNCIONANDO!"})
-    res.sendFile(path.join(__dirname, "./html/index.html"))
+    res.sendFile(path.join(__dirname, "../html", "index.html"));
 }
